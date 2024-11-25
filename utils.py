69a6b4e2bbe42689eafe434a2bddae56cb554f78
gaoshang <gaoshang@fawsn-ae.com>
2024-11-25 08:51:04 +0800

    The FreeTypeFont object from the PIL library no longer has the getsize() method, use the getbbox() method instead.

--- utils.py
+++ utils.py
@@ -4,15 +4,18 @@ from PIL import Image, ImageFont, ImageDraw, ImageOps
 
 def sort_chars(char_list, font, language):
     if language == "chinese":
-        char_width, char_height = font.getsize("制")
+        bbox  = font.getbbox("制")
     elif language == "korean":
-        char_width, char_height = font.getsize("ㅊ")
+        bbox  = font.getbbox("ㅊ")
     elif language == "japanese":
-        char_width, char_height = font.getsize("あ")
+        bbox  = font.getbbox("あ")
     elif language in ["english", "german", "french", "spanish", "italian", "portuguese", "polish"]:
-        char_width, char_height = font.getsize("A")
+        bbox  = font.getbbox("A")
     elif language == "russian":
-        char_width, char_height = font.getsize("A")
+        bbox  = font.getbbox("A")
+    char_width = bbox[2] - bbox[0]
+    char_height = bbox[3] - bbox[1]
+
     num_chars = min(len(char_list), 100)
     out_width = char_width * len(char_list)
     out_height = char_height
